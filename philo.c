01d23e76c4774e394e3c3b947d2d8ae53d797703
Sergio Garcia <sergio@atratopago.com>
2022-12-21 21:30:07 -0600

    ckpt init args

--- philo.c
+++ philo.c
@@ -6,16 +6,31 @@
 /*   By: segarcia <segarcia@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 19:17:33 by segarcia          #+#    #+#             */
-/*   Updated: 2022/12/21 20:47:14 by segarcia         ###   ########.fr       */
+/*   Updated: 2022/12/21 21:04:22 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static void	init_args(t_data *data, int argc, char **argv)
+{
+	data->n_philo = ft_atoi(argv[1]);
+	data->t_die = ft_atoi(argv[2]);
+	data->t_eat = ft_atoi(argv[3]);
+	data->t_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->n_must_eat = ft_atoi(argv[5]);
+	else
+		data->n_must_eat = -1;
+}
+
 int	main(int argc, char **argv)
 {
+	t_data	data;
+
 	if (valid_argcv(argc, argv) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
+	init_args(&data, argc, argv);
 	printf("Success \n");
 	return (EXIT_SUCCESS);
 }
